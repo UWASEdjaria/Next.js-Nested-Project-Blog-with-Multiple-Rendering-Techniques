5d34235804f23b27c20d7c01f188d5fd124a5eb8
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-05 16:14:24 +0200

    update layout styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,18 +20,21 @@ export default function RootLayout({
       <body
         className="flex min-h-screen flex-col  bg-gray-100 p-4">
 
-      <header className="w-full bg-black p-4 rounded-md flex items-center justify-between">
+      <header className="w-full border border-purple-400 rounded-lg bg-black p-4  flex items-center justify-between">
         
         <h1 className="text-purple-400 text-2xl font-bold">Nested Blog</h1>
         <nav className="flex gap-6">
-          <Link href='/'>Home</Link>
+          <Link
+           href='/'
+           className="text-white hover:text-purple-600 font-semibold transition-colors">Home
+           </Link>
           <Link
                 href='/about'
-             className="text-purple-400 hover:text-purple-600 font-semibold transition-colors">About
+             className="text-white hover:text-purple-600 font-semibold transition-colors">About
           </Link>
           <Link
                 href="/blog"
-                className="text-purple-400 hover:text-purple-600 font-semibold transition-colors">Blog
+                className="text-white hover:text-purple-600 font-semibold transition-colors">Blog
           </Link>
 
         </nav>
