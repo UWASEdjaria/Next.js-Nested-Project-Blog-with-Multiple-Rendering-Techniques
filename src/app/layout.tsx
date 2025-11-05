078edec9be19728bb78113d8b3dce87b7d62035c
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-05 14:26:39 +0200

    add style to layout and add page on [id]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";;
+import type { Metadata } from "next";
 import "./globals.css";
 import Link from "next/link";
 
@@ -18,11 +18,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4"
-      >
-        <Link href='/'>Home</Link>
-        <Link href='/About'>About</Link>
-        <Link href='/Blo'>Blog</Link>
+        className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4">
+
+      <header className="w-full bg-black p-4 rounded-md flex items-center justify-between">
+        
+        <h1 className="text-purple-400 text-2xl font-bold">Nested Blog</h1>
+        <nav className="flex gap-6">
+          <Link href='/'>Home</Link>
+          <Link
+                href='/About'
+             className="text-purple-400 hover:text-purple-600 font-semibold transition-colors">About
+          </Link>
+          <Link
+                href="/blog"
+                className="text-purple-400 hover:text-purple-600 font-semibold transition-colors">Blog
+          </Link>
+
+        </nav>
+        </header>
         {children}
       </body>
     </html>
