858ca0c3caf2a4c1dacdea79615fb7e16e088914
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-05 14:31:55 +0200

    update home page with content and style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      
+       <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold text-purple-600">Welcome to Our Blog</h1>
+      <p className="mt-4 text-black">Explore articles on technology, lifestyle, and education.</p>
+    </div>
     </div>
   )
 }
