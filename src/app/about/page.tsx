cc4d17cbbd15175105e8ae123f43592149f03b8d
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-05 15:25:30 +0200

    update the about page with styles and content

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className="max-w-3xl mx-auto p-4">
+      <h1 className="text-2xl font-bold text-purple-600">About the Author</h1>
+      <p className="mt-2 text-black">Learn more about the author and the inspiration behind this blog.</p>
+    </div>
+  )
+}
+
+export default page
