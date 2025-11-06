e9ec84b80ff258df5a7531c75aa3517d16338df5
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-06 09:24:09 +0200

    update the blogPage with fetching and styles

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
-
-function Blog() {
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+export default async function Blog() {
+  let posts:Post[]=[];
+  try {
+    const res =await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+    if(!res.ok){
+      throw new Error('Failed to fetch posts data');
+    }
+    posts=await res.json();
+  } catch (error) {
+    console.log(error);
+  }
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      <h1 className="text-2xl font-bold text-purple-600">Latest Posts</h1>
-      <p className="mt-2 text-white">Browse through our collection of articles and tutorials.</p>
+    <div className="flex flex-col  justify-center items-center min-h-screen">
+      <h1 className="text-2xl font-bold text-purple-600 m-5">Blog posts</h1>
+      {posts.map((post)=>(
+        <div key={post.id}
+        className='border border-purple-400 p-4 m-2 rounded-lg w-full md:w-3/4 shadow-lg'>
+          <h2 className=' text-pink-300 text-center font-bold italic p-1'>{post.title}</h2>
+          <p className='text-gray-400'>{post.body}</p>
+        </div>
+      ))}
     </div>
+
   )
 }
-
-export default Blog
