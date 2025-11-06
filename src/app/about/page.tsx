5fb612ce25d9797489087f5fd55ab8c7d494fd66
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-06 07:38:16 +0200

    update about page with fetching

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
 
-function page() {
+
+
+export default async function About() {
+  let data;
+  try {
+  const res= await fetch('https://jsonplaceholder.typicode.com/users/1');
+    if(!res.ok){
+      throw new Error('Failed to fetch user data');
+    }
+       data=await res.json();
+  
+   } catch (error) {
+    console.log(error);
+   }
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
       <h1 className="text-2xl font-bold text-purple-600">About the Author</h1>
-      <p className="mt-2 text-white">Learn more about the author and the inspiration behind this blog.</p>
+      <p className="mt-2 text-white">Name:{data.name}</p>
+      <p className="mt-2 text-white">Email:{data.email}</p>
+      <p className="mt-2 text-white">street{data.address.street}</p>
+      <p className="mt-2 text-white">city{data.address.city}</p>
+      <p className="mt-2 text-white">phone{data.phone}</p>
+      
     </div>
   )
 }
 
-export default page
